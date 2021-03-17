c86f562a2c8ffeb65642bbd94e9baad02052e042
Alexandre Bury <alexandre.bury@gmail.com>
2021-03-17 09:09:35 -0700

    Re-use asset for block tests.

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -60,49 +60,7 @@ mod tests {
     fn test_direct() {
         // Can we include_str!("assets/example.txt")?
         // It's excluded from the packaging step, so maybe not.
-        let text = r#"
-            ’Twas brillig, and the slithy toves
-            Did gyre and gimble in the wade;
-            All mimsy were the borogoves,
-            And the mome raths outgrabe.
-
-
-            "Beware the Jabberwock, my son!
-            The jaws that bite, the claws that catch!
-            Beware the Jubjub bird, and shun
-            The frumious Bandersnatch!"
-
-
-            He took his vorpal sword in hand:
-            Long time the manxome foe he sought—
-            So rested he by the Tumtum tree,
-            And stood awhile in thought.
-
-
-            And as in uffish thought he stood,
-            The Jabberwock, with eyes of flame,
-            Came whiffling through the tulgey wood,
-            And burbled as it came!
-
-
-            One, two! One, two! And through and through
-            The vorpal blade went snicker-snack!
-            He left it dead, and with its head
-            He went galumphing back.
-
-
-            "And hast thou slain the Jabberwock?
-            Come to my arms, my beamish boy!
-            O frabjous day! Callooh! Callay!"
-            He chortled in his joy.
-
-
-            ’Twas brillig, and the slithy toves
-            Did gyre and gimble in the wabe;
-            All mimsy were the borogoves,
-            And the mome raths outgrabe.
-        "#;
-
+        let text = include_str!("../../assets/example.txt");
         crate::test_cycle_unwrap(
             text.as_bytes(),
             |data| compress(data, 1),
