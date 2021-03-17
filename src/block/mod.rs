a874394f9b968526b7aab2f14c6321a063d89d3d
Alexandre Bury <alexandre.bury@gmail.com>
2021-03-16 22:28:44 -0700

    Replace test literal with public domain text

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -58,20 +58,50 @@ mod tests {
 
     #[test]
     fn test_direct() {
-        // hipsum.co
-        let text = "Pork belly art party wolf XOXO, neutra scenester ugh \
-                    thundercats tattooed squid skateboard beard readymade \
-                    kogi. VHS cardigan schlitz, meditation chartreuse kogi \
-                    tilde church-key. Actually direct trade hammock, \
-                    aesthetic VHS semiotics organic narwhal lo-fi heirloom \
-                    flexitarian master cleanse polaroid man bun. Flannel \
-                    helvetica mustache, bicycle rights small batch slow-carb \
-                    neutra tilde williamsburg meh poutine humblebrag. Four \
-                    dollar toast butcher actually franzen, gastropub \
-                    mustache tofu cardigan. 90's fingerstache forage \
-                    brooklyn meditation single-origin coffee tofu actually, \
-                    ramps pabst farm-to-table art party kombucha artisan \
-                    fanny pack. Flannel salvia ennui viral leggings selfies.";
+        // Can we include_str!("assets/example.txt")?
+        // It's excluded from the packaging step, so maybe not.
+        let text = r#"
+            ’Twas brillig, and the slithy toves
+            Did gyre and gimble in the wade;
+            All mimsy were the borogoves,
+            And the mome raths outgrabe.
+
+
+            "Beware the Jabberwock, my son!
+            The jaws that bite, the claws that catch!
+            Beware the Jubjub bird, and shun
+            The frumious Bandersnatch!"
+
+
+            He took his vorpal sword in hand:
+            Long time the manxome foe he sought—
+            So rested he by the Tumtum tree,
+            And stood awhile in thought.
+
+
+            And as in uffish thought he stood,
+            The Jabberwock, with eyes of flame,
+            Came whiffling through the tulgey wood,
+            And burbled as it came!
+
+
+            One, two! One, two! And through and through
+            The vorpal blade went snicker-snack!
+            He left it dead, and with its head
+            He went galumphing back.
+
+
+            "And hast thou slain the Jabberwock?
+            Come to my arms, my beamish boy!
+            O frabjous day! Callooh! Callay!"
+            He chortled in his joy.
+
+
+            ’Twas brillig, and the slithy toves
+            Did gyre and gimble in the wabe;
+            All mimsy were the borogoves,
+            And the mome raths outgrabe.
+        "#;
 
         crate::test_cycle_unwrap(
             text.as_bytes(),
